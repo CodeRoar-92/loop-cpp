33fad8f139878bddb455294cd62a88f15eee3494
CodeRoar-92 <coderoar92@gmail.com>
2024-02-24 07:11:18 +0500

    Simple loop structures

---
+++ loop-1st.cpp
@@ -0,0 +1,107 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=5; i++)//this is used for print row.
+//     {
+// for(int j=1; j<=5; j++){
+//     cout<<" * "; //used for coloumn print.
+// }
+// cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// second program 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=5; i++)/*this is used for print row. 1 2 3 4
+//                                                              1 2 3 4
+//                                                              The upper loop only 
+//                                                              iterate the loop then the inner loop 
+//                                                              executes the value which we are provide.*/ 
+//     {
+// for(int j=1; j<=5; j++){
+//     cout<<" "<<j; //used for column print. j iterate the value even that the value is true.
+// }
+// cout<<endl;
+//     }
+//     return 0;
+// }
+
+//Third program:
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=5; i++)//this is used for print row
+//     {
+// for(int j=1; j<=5; j++){
+//     cout<<" "<<i; //used for column print.
+// }
+// cout<<endl;
+//     }
+//     return 0;
+// }
+
+//Fourth program.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(char i='A'; i<='E'; i++)//this is used for print row.
+//     {
+// for(char  j='A'; j<='E'; j++){
+//     cout<<" "<<j; //used for column print.
+//     /*  A B C D E
+//         A B C D E*/
+
+// }
+// cout<<endl;
+//     }
+//     return 0;
+// }
+
+//fifth porgram
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i='A'; i<='E'; i++){
+//         for(int j='A'; j<='E'; j++){  /*output is 
+//                                        A A A A 
+//                                        B B B B //But is input was of ACS Code
+//                                        C C C C*/
+                              
+//             cout<<" "<<i;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//sixth program
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(char i ='E'; i>='A';i--){
+//         for(char j='E'; j>='A'; j--){ /*E D C B A
+//                                         E D C B A*/
+//             cout<<" "<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//7th program.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(char i = 'E'; i>='A'; i--){
+//         for(char j = 'E'; j>='A'; j--){
+//             cout<<" "<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
